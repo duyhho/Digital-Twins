88a8dad69b35af70f61cc04e3e63b3eef36c855e
aha85b <aha85b@umsystem.edu>
2023-11-19 22:00:21 -0600

    a chair is generated front of each table

--- Assets/Flarsheim Hall/Scripts/OfficeFurnitureGenerator.cs
+++ Assets/Flarsheim Hall/Scripts/OfficeFurnitureGenerator.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public class DataObject
+{
+    public int px;
+    public int ry;
+    public int pz;
+
+    // Constructor to initialize the object
+    public DataObject(int px, int ry, int pz)
+    {
+        this.px = px;
+        this.ry = ry;
+        this.pz = pz;
+    }
+}
 
 public class OfficeFurnitureGenerator : MonoBehaviour
 {
+    // Define a class for your data objects
+
+
+
 
     public List<GameObject> chairPrefabs;
     GameObject chair;
     public Transform parentObject;
     int randomIndex = 0;
+    public List<DataObject> dataObjectList = new List<DataObject>();
+
+    Vector3 rotationVector;
+    Quaternion rotationQuaternion;
+
+
+
     void Start()
     {
 
@@ -22,10 +51,33 @@ public class OfficeFurnitureGenerator : MonoBehaviour
         /*Generate new chairs*/
         Debug.Log("Button is clicked");
 
-        randomIndex = Random.Range(0, chairPrefabs.Count);
-        chair = Instantiate(chairPrefabs[randomIndex], parentObject) as GameObject;
-        chair.transform.parent = parentObject;
-        chair.transform.localPosition = new Vector3(0f, 0f, 0f);
+        // randomIndex = Random.Range(0, chairPrefabs.Count);
+        // chair = Instantiate(chairPrefabs[randomIndex], parentObject) as GameObject;
+        // chair.transform.parent = parentObject;
+        // chair.transform.localPosition = new Vector3(0f, 0f, 0f);
+
+        // for (int i = 0; i <= 8; i++)
+        // {
+        //     randomIndex = Random.Range(0, chairPrefabs.Count);
+        //     chair = Instantiate(chairPrefabs[randomIndex], parentObject) as GameObject;
+        //     chair.transform.parent = parentObject;
+        //     chair.transform.localPosition = new Vector3(i * 1.5f, 0f, i * 1.5f);
+        // }
+
+        foreach (DataObject dataObject in dataObjectList)
+        {
+            // Create a Vector3 representing the desired rotation (in degrees)
+            rotationVector = new Vector3(0f, dataObject.ry, 0f);
+
+            // Convert the Vector3 to a Quaternion using Quaternion.Euler
+            rotationQuaternion = Quaternion.Euler(rotationVector);
+            //Debug.Log($"{{x: {dataObject.x}, y: {dataObject.y}, z: {dataObject.z}}}");
+            randomIndex = Random.Range(0, chairPrefabs.Count);
+            chair = Instantiate(chairPrefabs[randomIndex], parentObject) as GameObject;
+            chair.transform.parent = parentObject;
+            chair.transform.localPosition = new Vector3(dataObject.px, 0f, dataObject.pz);
+            chair.transform.localRotation = rotationQuaternion;
+        }
     }
     private void DestroyAllChairsWithTag()
     {
